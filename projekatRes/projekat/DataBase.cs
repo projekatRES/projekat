8f0b9c69c7af9a81df67fc1cb53a6ea31ede4876
Aleksandra Kovac <kovacaleksandra19@gmail.com>
2018-06-02 18:32:10 +0200

    DataIO, DataBase

---
+++ projekatRes/projekat/DataBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekat
+{
+    public static class DataBase
+    {
+        public static DataIO serializer = new DataIO();
+    }
+}
