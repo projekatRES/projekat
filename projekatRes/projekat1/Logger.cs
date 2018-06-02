998829f424bd8d4f5c0e2e8b70309e520e5598b9
Aleksandra Kovac <kovacaleksandra19@gmail.com>
2018-06-02 14:23:18 +0200

    Implementacija Logger-a

---
+++ projekatRes/projekat1/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekat1
+{
+    public class Logger
+    {
+        public static void Log(string str)
+        {
+            try
+            {
+                File.AppendAllText("Logger.txt", str);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
